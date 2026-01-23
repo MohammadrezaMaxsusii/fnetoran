5704b04a0deed2b6ea17035305a8111f76fd8b4e
Hojat Nasiri <hnasiri1997@gmail.com>
2026-01-23 13:59:17 +0330

    fix: add device sidebar menu item

--- src/features/devices/schemas/deviceCreateSchema.ts
+++ src/features/devices/schemas/deviceCreateSchema.ts
@@ -1,16 +1,6 @@
 import { z } from "zod";
 
 export const deviceCreateSchema = z.object({
-  // type: z.enum(["allow", "block"]),
-  // removeDate: z.preprocess(
-  //   (val) => (typeof val === "string" && val ? new Date(val) : undefined),
-  //   z.date().optional()
-  // ),
-  // source: z.string().trim(),
-  // fileName: z.string().trim(),
-  // fileType: z.string().trim().optional(),
-  // item: z.ipv4(),
-
   deviceId: z.number().optional(),
   ip: z.ipv4().optional(),
   vendor: z.string().min(1, "Vendor is required!").optional(),
