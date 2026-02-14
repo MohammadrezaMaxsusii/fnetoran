e22271a37a98b38f8657f043affcf684de6235a9
Administrator <gitlab_admin_3f297b@example.com>
2026-02-14 16:54:24 +0330

    style: style error boundary again

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -10,17 +10,19 @@ export const Error = ({ error }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-screen h-screen grid place-content-center gap-5">
-      <ErrorIcon className="text-red size-40 mx-auto" />
-      <h1 className="text-3xl">Something went wrong!</h1>
-      {error && <p className="text-xl text-center">{error}</p>}
-      <Button
-        variant="secondary"
-        className="border border-default"
-        onClick={() => navigate(-1)}
-      >
-        Back to previous page
-      </Button>
+    <div className="w-screen h-screen grid place-content-center">
+      <div className="flex flex-col items-center justify-center gap-5">
+        <ErrorIcon className="text-red size-40 mx-auto" />
+        <h1 className="text-3xl">Something went wrong!</h1>
+        {error && <p className="text-xl text-center max-w-160">{error}</p>}
+        <Button
+          variant="secondary"
+          className="border border-default"
+          onClick={() => navigate(-1)}
+        >
+          Back to previous page
+        </Button>
+      </div>
     </div>
   );
 };
